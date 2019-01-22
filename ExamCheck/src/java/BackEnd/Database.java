904eed5f3ca9c77821d657b3e4ed2d6863bbb2c1
ryanl13214 <rlangridge132@hotmail.com>
2019-01-22 14:44:33 +0000

    fixed error with last commit

--- ExamCheck/src/java/BackEnd/Database.java
+++ ExamCheck/src/java/BackEnd/Database.java
@@ -5,8 +5,14 @@
  */
 package BackEnd;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;

d85fa5e90fda4c665f0c83aa698df3e8906f4546
ryanl13214 <rlangridge132@hotmail.com>
2019-01-24 10:23:30 +0000

    Update Database.java

--- ExamCheck/src/java/BackEnd/Database.java
+++ ExamCheck/src/java/BackEnd/Database.java
@@ -225,15 +225,103 @@ public String getoffice(String pk){
             return "0";
         }  
   }
+public String number_of_completed_exams(String pk){
+  try{
+            String sql ="select count(*) from exams where ExmVetComit_exmVet_pk=2;";
+            Statement state = conn.createStatement();
+
+            ResultSet rs = state.executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();    
+            
+            
+                     return  rs.getString(1);
+            
+            
+        }
+        catch (SQLException ex) {
+            return "0";
+        }  
+
+
+}
+public String number_of_in_progress_exams(String pk){
+try{
+            String sql ="select count(*) from exams where exams_External_Examiner_FK!=2 and exams_ExmVetComit_FK!=2 and exams_internal_moderator_FK!=2 and exams_External_Examiner_FK!=1 and exams_ExmVetComit_FK!=1 and exams_internal_moderator_FK!=1;";
+            Statement state = conn.createStatement();
+
+            ResultSet rs = state.executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();               
+            return  rs.getString(1);            
+        }
+        catch (SQLException ex) {
+            return "0";
+        }  
+
+
+
+}
+public String number_of_new_exams(String pk){
+try{
+            String sql ="select count(*) from exams where exams_External_Examiner_FK=1 and exams_ExmVetComit_FK=1 and exams_internal_moderator_FK=1;";
+            Statement state = conn.createStatement();
+
+            ResultSet rs = state.executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();               
+            return  rs.getString(1);            
+        }
+        catch (SQLException ex) {
+            return "0";
+        }  
+
+
+}
 
 
 
 
-     public String blobin(String path,String Modulecode,String level, String pk,String title, String online,String resit, String exam_setter_lect_pk){
+public boolean changeStage(String pk){
+try{
+     Statement state = conn.createStatement();
+
+            state.executeUpdate("");
+
+            return true;
+        } catch (SQLException ex) {
+            // handle any sql errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return false;
+        }
+
+}
+/*
+public ResultSet view_unsigned_exams(){
+
+    try {
+            Statement state = conn.createStatement();
+
+            ResultSet rs = state.executeQuery(query);
+
+            return rs;
+        } catch (SQLException ex) {
+            // handle any sql errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return null;
+        }
+}
+*/
+
+     public String blobin(InputStream inputStream ,String Modulecode,String level, String pk,String title, String online,String resit, String exam_setter_lect_pk){
         try{
               
            
-                InputStream inputStream = new FileInputStream(new File(path));
+         //       InputStream inputStream = new FileInputStream(new File(path));
                 String sql = "INSERT INTO `18agileteam7db`.`exams`(`exam_pk`,`module_code`,`title`,`online_or_paper`,`resit`,`examFile`,`exam_setter_lect_pk`,`internal_moderator_int_mod_pk`,`External_Examiner_ext_exam_pk`,`ExmVetComit_exmVet_pk`)VALUES("+pk+","+Modulecode+","+title+","+online+","+resit+","+"?"+exam_setter_lect_pk+"1,1,1);";
                 //String sql = "INSERT INTO `18agileteam7db`.`entity_1`(`PK`,`test`)VALUES(8,null);";
                 
@@ -249,8 +337,11 @@ public String getoffice(String pk){
         }
         return "end of blob in";
     }
+     
+     
+     
 
-        public void blobout(String exampk,String path,String name){
+        public File blobout(String exampk,String path,String name){
         try{
             Statement state = conn.createStatement();
             ResultSet rset = state.executeQuery("select examFile from exams where exam_pk="+exampk+";");
@@ -258,14 +349,18 @@ public String getoffice(String pk){
             Blob blob;
             int i=1;
             String doctype =  rset.getString("doctype");
-            File f=new File(path+"\\"+name+doctype);
+            File f=new File("C:\\"+name+doctype);
             FileOutputStream fs = new FileOutputStream(f);
             blob=rset.getBlob("test");
             b=blob.getBytes(1, (int)blob.length());
             fs.write(b);
+         return f;
         }
         catch(Exception e){
             System.out.println(e);
+           File v= new File("Csbxfgfgn");
+           return v;
         }
+        
     }
 }

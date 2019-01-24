ccaefe5d17273975d1f97e31a737428f2ed89169
MattMcN <mattmcn@btinternet.com>
2019-01-24 17:04:18 +0000

    Repaired FileUpload.java

---
+++ ExamCheck/src/java/BackEnd/FileUpload.java
@@ -0,0 +1,122 @@
+
+package BackEnd;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import BackEnd.Database;
+/**
+ *
+ * @author matthewmcneil
+ */
+@MultipartConfig
+public class FileUpload extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        Part filePart = request.getPart("file");
+        
+        //get inputs
+        //InputStream fileContent = filePart.getInputStream();
+
+        String filePath = request.getParameter("fileToUpload");
+        String moduleCode = request.getParameter("moduleCode");
+        String moduleTitle = request.getParameter("moduleTitle");
+        String examType = request.getParameter("examType");
+        String examChoice = request.getParameter("examChoice");
+        String examLevel = request.getParameter("examLevel");
+        
+        out.println(filePath);
+        out.println(moduleCode);
+        out.println(moduleTitle);
+        out.println(examType);
+        out.println(examChoice);
+        out.println(examLevel);
+        
+        //Connect to database
+        Database db = new Database();
+        db.connect();
+        
+        //
+        InputStream inputStream = new FileInputStream(new File(filePath));
+        out.println(inputStream);
+        //db.updateQuery("INSERT INTO `18agileteam7db`.`entity_1`(`PK`,`test`)VALUES(134,null);");
+        db.blobin( inputStream, moduleCode, examLevel, "34", moduleTitle, examType, examChoice, "1");
+        out.println("end");
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet createAccount</title>");
+            out.println("File passed");
+            out.println("</head>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

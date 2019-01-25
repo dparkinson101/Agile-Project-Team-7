c88bc394cd5f363ae16f9c1d6762ba1158a32db4
ryanl13214 <rlangridge132@hotmail.com>
2019-01-25 11:27:34 +0000

    added update blob function

--- ExamCheck/src/java/BackEnd/Database.java
+++ ExamCheck/src/java/BackEnd/Database.java
@@ -485,6 +485,37 @@ String sql = "update exams set External_Examiner_ext_exam_pk=2 ="+pk+";";
         }
         return "end of blob in";
     }
+     
+     
+     
+          public String blobintwo(InputStream inputStream, String docType,String pk){
+        try{
+           
+           //     InputStream inputStream = new FileInputStream(new File(path));         
+          //  String sql = "INSERT INTO `18agileteam7db`.`exams`(`exam_pk`,`module_code`,`title`,`online_or_paper`,`resit`,`exam`,`grade`,`examFile`,`doctype`,`exam_setter_lect_pk`,`internal_moderator_int_mod_pk`,`External_Examiner_ext_exam_pk`,`ExmVetComit_exmVet_pk`)VALUES(\""+examPK+"\",\""+Modulecode+"\",\""+title+"\",\""+online+"\",\""+resit+"\",\"1\",\""+level+"\",?,\""+docType+"\",1,1,1,1);";
+           String sql = "update exams set doctype ="+docType+" , set examFile =? where exam_pk ="+pk+" ;";
+          //   String sql = "INSERT INTO `18agileteam7db`.`entity_1`(`PK`,`test`)VALUES(134,?);";
+                
+        //        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssss");
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setBlob(1, inputStream);
+                statement.executeUpdate();
+            }
+       
+        catch(SQLException e){
+          System.out.println("error");
+          System.out.println(e);
+            return e.toString();
+        }
+        return "end of blob in";
+    }
+     
+     
+     
+     
+     
+     
+     
     public ResultSet info_examslinkedtopkvetcommit(String pk) {
         try {
             String sql = "select * from exams where ExmVetComit_exmVet_pk=" + pk + ";";

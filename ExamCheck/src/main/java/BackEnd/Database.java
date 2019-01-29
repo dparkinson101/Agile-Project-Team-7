cb2871e939fef880c33593569cb1f611edb79986
dparkinson <dparkinson@dundee.ac.uk>
2019-01-29 15:32:13 +0000

    Converted Project to Maven & Tomcat Server

---
+++ ExamCheck/src/main/java/BackEnd/Database.java
@@ -0,0 +1,585 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackEnd;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Douglas
+ */
+public class Database {
+
+    private Connection conn;
+
+    public Database() {
+        conn = null;
+    }
+
+    public void close() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     *
+     * @return Returns an instance of the sql.Connection class which is the
+     * current connection to the database.
+     */
+    public Connection connect() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+            System.out.println("Failed to register MySQL Connector/J");
+            System.out.println(ex);
+            return null;
+        }
+
+        try {
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://silva.computing.dundee.ac.uk:3306/18agileteam7db", "18agileteam7", "8302.at7.2038");
+            return conn;
+        } catch (SQLException ex) {
+            // handle any sql errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param query SQL query for the database to process.
+     * @return Returns results of query for the database in a sql.ResultSet
+     * object.
+     */
+    public ResultSet executeQuery(String query) {
+        try {
+            this.connect();
+            Statement state = conn.createStatement();
+
+            ResultSet rs = state.executeQuery(query);
+            return rs;
+        } catch (SQLException ex) {
+            // handle any sql errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            this.close();
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param query SQL query for the database to process.
+     * @return Returns results of query for the database in a sql.ResultSet
+     * object.
+     */
+    public boolean updateQuery(String query) {
+        try {
+            this.connect();
+            Statement state = conn.createStatement();
+
+            state.executeUpdate(query);
+
+            this.close();
+            return true;
+        } catch (SQLException ex) {
+            // handle any sql errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            this.close();
+            return false;
+        }
+    }
+
+    public void movetoexamvettingcommite(String pk) {
+
+        String sql = "update exams set internal_moderator_int_mod_pk=2 where exam_pk=" + pk + ";"; // handle any sql errors
+        updateQuery(sql);
+
+    }
+
+    public void movetoexamvetexternalmodderator(String pk) {
+
+        String sql = "update exams set Exam_Vetting_Committee_exmVet_pk=2 where exam_pk=" + pk + ";";
+        updateQuery(sql);
+
+    }
+
+    public void finish_exam(String pk) {
+
+        String sql = "update exams set External_Examiner_ext_exam_pk=2 where exam_pk = " + pk + ";";
+        updateQuery(sql);
+
+    }
+
+    public void addcomment(String comments, String pk, String date, int pointer) {
+
+        if (pointer == 1) {
+
+            this.movetoexamvettingcommite(pk);
+        }
+
+        if (pointer == 2) {
+
+            this.movetoexamvetexternalmodderator(pk);
+
+        }
+        if (pointer == 3) {
+
+            this.finish_exam(pk);
+        }
+
+        // INSERT INTO `18agileteam7db`.`comments`(`comments_pk`,`commentssssss`,`Attribute_3`,`exams_exam_pk`)VALUES(1,"a","a",15758);
+        String sql = "INSERT INTO `18agileteam7db`.`comments`(`comments_pk`,`commentssssss`,`Attribute_3`,`exams_exam_pk`)VALUES(" + pk + pointer + ",\"" + comments + "\",\"" + date + "\"," + pk + ");";
+        updateQuery(sql);
+    }
+
+    /**
+     *
+     * @param username the username / email of the user
+     * @param password the password of the user
+     * @return Returns the user_pk of the if they are a valid user in the
+     * database
+     */
+    public String checkLogin(String username, String password) {
+        try {
+            this.connect();
+            if (username == null || password == null) {
+                this.close();
+                return null;
+            }
+
+            String sql = "select user_pk, password, salt from users where username = '" + username + "';";
+
+            if (conn == null) {
+                System.out.println("The connection is null");
+                this.close();
+            }
+
+            Base64.Decoder dec = Base64.getDecoder();
+
+            ResultSet rs = executeQuery(sql);
+
+            if (rs == null) {
+                System.out.println("No Results for such credentials exist in the database.");
+                this.close();
+                return null;
+            }
+
+            rs.first();
+
+            byte[] salt = dec.decode(rs.getString("salt"));
+            byte[] saltedHash = dec.decode(rs.getString("password"));
+            byte[] passwordHash = null;
+
+            Security security = new Security();
+            try {
+                passwordHash = security.getSaltedHash(password, salt);
+            } catch (Exception e) {
+                System.out.println("ERROR: PASSWORD CHECK FAILED TO GET SALTED HASH");
+                System.out.println(e);
+            }
+
+            String user_pk = rs.getString(1);
+
+            if (Arrays.equals(passwordHash, saltedHash) && user_pk != null) {
+                System.out.println("User: " + username + " Logged in successfully!");
+                System.out.println("UserPK: " + user_pk);
+
+                return user_pk;
+            } else {
+                System.out.println("User: " + username + " Log in failed!");
+                this.close();
+                return null;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Database Erorr");
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            this.close();
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String getexamsetter(String pk) {
+        try {
+            String sql = "select lect_pk from exam_setter where user_user_pk =" + pk + ";";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+
+            return rs.getString(1);
+
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String getinternalmod(String pk) {
+        try {
+            String sql = "select int_mod_pk from internal_moderator where user_user_pk =" + pk + ";";
+
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+
+            return rs.getString(1);
+
+        } catch (SQLException ex) {
+            return "0";
+        } catch (Exception e) {
+            System.out.println("Something broke.");
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String getexamvetcommit(String pk) {
+        try {
+            String sql = "select exmVet_pk from ExmVetComit where user_user_pk =" + pk + ";";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String getexternal(String pk) {
+        try {
+            String sql = "select ext_exam_pk from External_Examiner where user_user_pk =" + pk + ";";
+
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+
+            return rs.getString(1);
+
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String getoffice(String pk) {
+        try {
+            String sql = "select school_office_pk from School_office where user_user_pk =" + pk + ";";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String number_of_completed_exams(String pk) {
+        try {
+            String sql = "select count(*) from exams where ExmVetComit_exmVet_pk=2;";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+
+        } catch (SQLException ex) {
+            return "0";
+        }
+
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String number_of_in_progress_exams(String pk) {
+        try {
+            String sql = "select count(*) from exams where External_Examiner_ext_exam_pk!=2 and ExmVetComit_exmVet_pk!=2 and internal_moderator_int_mod_pk!=2 and External_Examiner_ext_exam_pk!=1 and ExmVetComit_exmVet_pk!=1 and internal_moderator_int_mod_pk!=1;";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+    public String number_examslinkedtopk(String pk) {
+        try {
+            String sql = "select count(*) from exams where exam_setter_lect_pk=" + pk + ";";
+
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    public ResultSet info_examslinkedtopk(String pk) {
+        String sql = "select * from exams where exam_setter_lect_pk=" + pk + ";";
+        ResultSet rs = executeQuery(sql);
+        return rs;
+    }
+
+    public String number_examslinkedtopkintmod(String pk) {
+        try {
+            String sql = "select count(*) from exams where internal_moderator_int_mod_pk=" + pk + ";";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    public ResultSet info_examslinkedtopkintmod(String pk) {
+        String sql = "select * from exams where internal_moderator_int_mod_pk=" + pk + ";";
+        ResultSet rs = executeQuery(sql);
+        return rs;
+    }
+
+    public String number_examslinkedtopkextmod(String pk) {
+        try {
+            String sql = "select count(*) from exams where External_Examiner_ext_exam_pk=" + pk + ";";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    public ResultSet info_examslinkedtopkextmod(String pk) {
+        String sql = "select * from exams where External_Examiner_ext_exam_pk=" + pk + ";";
+        ResultSet rs = executeQuery(sql);
+        return rs;
+    }
+
+    public String number_examslinkedtopkvetcommit(String pk) {
+        try {
+            String sql = "select count(*) from exams where ExmVetComit_exmVet_pk=" + pk + ";";
+
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+    }
+
+    /**
+     *
+     * @param inputStream
+     * @param Modulecode
+     * @param level
+     * @param pk
+     * @param title
+     * @param online
+     * @param resit
+     * @param exam_setter_lect_pk
+     * @param examPK
+     * @param docType
+     * @return
+     */
+    public String blobin(InputStream inputStream, String Modulecode, String level, String pk, String title, String online, String resit, String exam_setter_lect_pk, String examPK, String docType) {
+        try {
+
+            String sql = "INSERT INTO `18agileteam7db`.`exams`(`exam_pk`,`module_code`,`title`,`online_or_paper`,`resit`,`exam`,`grade`,`examFile`,`doctype`,`exam_setter_lect_pk`,`internal_moderator_int_mod_pk`,`External_Examiner_ext_exam_pk`,`ExmVetComit_exmVet_pk`)VALUES(\"" + examPK + "\",\"" + Modulecode + "\",\"" + title + "\",\"" + online + "\",\"" + resit + "\",\"1\",\"" + level + "\",?,\"" + docType + "\",1,1,1,1);";
+
+            connect();
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setBlob(1, inputStream);
+                statement.executeUpdate();
+            close();
+        } catch (SQLException e) {
+            System.out.println("error");
+            System.out.println(e);
+            close();
+            return e.toString();
+        }
+        close();
+        return "end of blob in";
+    }
+
+    public String updateblob(InputStream inputStream, String docType, String pk) {
+
+        try {
+            connect();
+            String sql = "update exams set doctype ='" + docType + "' , examFile =? where exam_pk =" + pk + " ;";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setBlob(1, inputStream);
+
+            statement.executeUpdate();
+            close();
+        } catch (SQLException e) {
+
+            System.out.println("error");
+
+            System.out.println(e);
+
+            close();
+            return e.toString();
+        }
+        close();
+        return "end of blob in";
+
+    }
+
+    public ResultSet info_examslinkedtopkvetcommit(String pk) {
+        String sql = "select * from exams where ExmVetComit_exmVet_pk=" + pk + ";";
+        ResultSet rs = executeQuery(sql);
+        return rs;
+    }
+
+    public String number_of_new_examsextmod(String pk) {
+        try {
+            String sql = "select count(*) from exams where External_Examiner_ext_exam_pk=1 AND ExmVetComit_exmVet_pk=1 AND internal_moderator_int_mod_pk=1;";
+            ResultSet rs = executeQuery(sql);
+            rs.beforeFirst();
+            rs.next();
+            return rs.getString(1);
+        } catch (SQLException ex) {
+            return "0";
+        }
+
+    }
+
+    public int gettotalnumberofexams() {
+        String sql = "select count(*) from exams;";
+        ResultSet rs = executeQuery(sql);
+        try {
+            return rs.getInt(0);
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+
+    /**
+     *
+     * @param pk
+     * @return
+     */
+//    public boolean changeStage(String pk) {
+//        try {
+//            Statement state = conn.createStatement();
+//
+//            state.executeUpdate("");
+//
+//            return true;
+//        } catch (SQLException ex) {
+//            // handle any sql errors
+//            System.out.println("SQLException: " + ex.getMessage());
+//            System.out.println("SQLState: " + ex.getSQLState());
+//            System.out.println("VendorError: " + ex.getErrorCode());
+//            return false;
+//        }
+//
+//    }
+//    public ResultSet view_unsigned_exams() {
+//
+//        try {
+//            Statement state = conn.createStatement();
+//
+//            ResultSet rs = state.executeQuery(query);
+//
+//            return rs;
+//        } catch (SQLException ex) {
+//            // handle any sql errors
+//            System.out.println("SQLException: " + ex.getMessage());
+//            System.out.println("SQLState: " + ex.getSQLState());
+//            System.out.println("VendorError: " + ex.getErrorCode());
+//            return null;
+//        }
+//    }
+    /**
+     *
+     * @param exampk
+     * @return
+     */
+//    public Blob blobout(String exampk) {
+//        try {
+//            Statement state = conn.createStatement();
+//            ResultSet rset = state.executeQuery("select examFile from exams where exam_pk=" + exampk + ";");
+//            byte b[];
+//            Blob blob;
+//            int i = 1;
+//            // String doctype = rset.getString("doctype");
+//            //File f = new File(path + "\\" + name + doctype);
+//            //FileOutputStream fs = new FileOutputStream(f);
+//            blob = rset.getBlob("examFile");
+//
+//            return blob;
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//            File v = new File("Csbxfgfgn");
+//            return null;
+//        }
+//
+//    }
+}
---
+++ ExamCheck/src/main/webapp/Log-in.jsp
@@ -0,0 +1,83 @@
+<%-- 
+    Document   : Log-in
+    Created on : 22-Jan-2019, 10:54:27
+    Author     : andrewbrodrick
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Log-In</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+        <style type="text/css">
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+        </style>
+        <script>
+            function deleteAllCookies() {
+                var cookies = document.cookie.split(";");
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = cookies[i];
+                    var eqPos = cookie.indexOf("=");
+                    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <div class="login-form">
+            <form action="loginManager" method="post">
+                <h2 class="text-center">Log in</h2>       
+                <div class="form-group">
+                    <input type="text" name="email" class="form-control" placeholder="Email" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required="required">
+                </div>
+                <%
+                    Cookie[] cookies = request.getCookies();
+                    for (Cookie c : cookies) {
+                        if (c.getName().equals("login")) {
+                            if (c.getValue().equals("false")) {
+                                out.println("<p class='text-danger text-center'> Incorrect Login Credentials <br> Please try again</p>");
+                            }
+                        }
+                    }
+                %>
+                <div class="form-group">
+                    <button type="submit" onclick="deleteAllCookies()" class="btn btn-primary btn-block">Log in</button>
+                </div>
+                <div class="clearfix">
+                    <a href="#" class="pull-right">Forgot Password?</a>
+                </div>        
+            </form>
+            <p class="text-center"><a href="/ExamCheck/createAccount.jsp">Create an Account</a></p>
+        </div>
+    </body>
+</html>
